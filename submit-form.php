2817490300b399afdc5904f31ef3d7e388fb7657
tala12397 <105044125+tala12397@users.noreply.github.com>
2023-03-22 16:18:34 +0200

    Update submit-form.php

--- submit-form.php
+++ submit-form.php
@@ -1,93 +1,56 @@
-<!-- $to = "abrahamt@post.bgu.ac.il";
-$subject = "New form submission";
-$headers = "From: webmaster@example.com\r\n";
-$headers .= "Reply-To: ".$_POST["email"]."\r\n";
-$headers .= "Content-type: text/html\r\n";
-$name = $_POST["name"];
-$email = $_POST["email"];
-$message = $_POST["message"];
-$body = "<h2>New form submission</h2>";
-$body .= "<p><strong>Name:</strong> ".$name."</p>";
-$body .= "<p><strong>Email:</strong> ".$email."</p>";
-$body .= "<p><strong>Message:</strong><br>".$message."</p>";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(mail($to, $subject, $body, $headers)) {
-    echo "Thank you for your message!";
+<?php
+
+// Check if form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+  // Get form data
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $smiley = $_POST['smiley'];
+
+  // Validate email
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $error_message = 'Please enter a valid email address.';
+  }
+
+  // Validate message
+  if (empty($message)) {
+    $error_message = 'Please enter a message.';
+  }
+
+  // If there are no errors, send email
+  if (empty($error_message)) {
+
+    // Set recipient email address
+    $to = 'abrahamt@post.bgu.ac.il';
+
+    // Set email subject
+    $subject = 'New message from contact form';
+
+    // Set email message
+    $email_message = "From: $email\n\n";
+    $email_message .= "Message:\n$message\n\n";
+    $email_message .= "Smiley: $smiley";
+
+    // Set email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Send email
+    if (mail($to, $subject, $email_message, $headers)) {
+      $success_message = 'Your message has been sent.';
     } else {
-    echo "There was a problem sending your message. Please try again later.";
+      $error_message = 'There was a problem sending your message. Please try again.';
     }
+  }
 }
-else{
-    echo "problem"
-} -->
 
-<?php
-if (isset($_POST['Email'])) {
-
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "abrahamt@post.bgu.ac.il";
-    $email_subject = "New form submissions";
-
-    function problem($error)
-    {
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br><br>";
-        echo $error . "<br><br>";
-        echo "Please go back and fix these errors.<br><br>";
-        die();
-    }
-
-    // validation expected data exists
-    if (
-        !isset($_POST['Email']) ||
-        !isset($_POST['Message'])
-    ) {
-        problem('We are sorry, but there appears to be a problem with the form you submitted.');
-    }
-
-    $email = $_POST['Email']; // required
-    $message = $_POST['Message']; // required
-
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-    if (!preg_match($email_exp, $email)) {
-        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
-    }
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-
-    if (strlen($message) < 2) {
-        $error_message .= 'The Message you entered do not appear to be valid.<br>';
-    }
-
-    if (strlen($error_message) > 0) {
-        problem($error_message);
-    }
-
-    $email_message = "Form details below.\n\n";
-
-    function clean_string($string)
-    {
-        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
-        return str_replace($bad, "", $string);
-    }
-
-    $email_message .= "Email: " . clean_string($email) . "\n";
-    $email_message .= "Message: " . clean_string($message) . "\n";
-
-    // create email headers
-    $headers = 'From: ' . $email . "\r\n" .
-        'Reply-To: ' . $email . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-    @mail($email_to, $email_subject, $email_message, $headers);
 ?>
 
-    <!-- include your success message below -->
-
-    Thank you for contacting us. We will be in touch with you very soon.
-
-<?php
-}
-?>
+<!-- PHP code to display success or error message -->
+<?php if (!empty($success_message)) { ?>
+  <p class="success"><?php echo $success_message; ?></p>
+<?php } ?>
+<?php if (!empty($error_message)) { ?>
+  <p class="error"><?php echo $error_message; ?></p>
+<?php } ?>
